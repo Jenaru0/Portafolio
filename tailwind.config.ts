9c53f793f869733f3956381a5c5047751d900797
Jesus Ruiz <2201080232@undc.edu.pe>
2025-01-28 19:30:08 -0500

    primer commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
+  darkMode: 'class', // Habilitamos dark mode manual
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +16,11 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: false, // Desactivamos temas predefinidos de DaisyUI
+    darkTheme: "dark", // Forzamos coincidencia con nuestro sistema
+    base: true,
+    styled: true,
+  },
 } satisfies Config;
